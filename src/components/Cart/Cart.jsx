a7b19b80b2cb49d73d0d8c75adf869f5c13da321
selima <musaevas203@gmail.com>
2024-03-28 13:42:54 +0500

    1 product

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import img1 from '../../assets/img1.jpeg'
+
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div className='Cart'>
+      <h1>Shopping Cart</h1>
+      <img src={img1} alt="img" />
+      <p className='price'>$ 50</p>
+    </div>
   )
 }
 
